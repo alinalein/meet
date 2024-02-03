55c8f52977f684af8e172753214eb083429a6e42
alilein1990 <alina.leinweber@gmx.de>
2024-02-03 23:41:46 +0100

    add final test -scenario 2

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,16 +1,46 @@
 import { useState } from "react";
+import { extractLocations, getEvents } from '../api';
 
-const CitySearch = () => {
+
+const CitySearch = ({ allLocations }) => {
     const [showSuggestions, setShowSuggestions] = useState(false);
+    // for the value in input
+    const [query, setQuery] = useState("");
+    // will suggest in li only cities that will match the input field
+    const [suggestions, setSuggestions] = useState([]);
+
+    const handleInputChanged = (event) => {
+        const value = event.target.value;
+        const filteredLocations = allLocations ? allLocations.filter((location) => {
+            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        }) : [];
+
+        setQuery(value);
+        setSuggestions(filteredLocations);
+    };
+
+
     return (
         <div id="city-search">
             <input
                 type="text"
                 className="city"
                 placeholder="Search for a city"
+                value={query}
                 onFocus={() => setShowSuggestions(true)}
+                onChange={handleInputChanged}
             />
-            {showSuggestions ? <ul className="suggestions"></ul> : null}
+            {showSuggestions ?
+                <ul className="suggestions">
+                    {suggestions.map((suggestion) => {
+                        // return list if location we suggest based on input 
+                        return <li key={suggestion}>{suggestion}</li>
+                    })}
+                    <li key='See all cities'>
+                        <b>See all cities</b>
+                    </li>
+                </ul>
+                : null}
         </div>
     )
 }
