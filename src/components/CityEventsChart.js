e349369df2e3561e24abdc305c674a1f53ac49a8
alilein1990 <alina.leinweber@gmx.de>
2024-02-20 23:01:17 +0100

    adjusted chart 1

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -22,8 +22,8 @@ const CityEventsChart = ({ allLocations, events }) => {
         const data = allLocations.map((location) => {
             // gets only events that have same location as location and get the length of result
             const count = events.filter((event) => event.location === location).length
-            // splits the location where the comma is and takes the first part, so only Berlin
-            const city = location.split(', ')[0]
+            // splits the location where , OR - is and takes the first part, so only city
+            const city = location.split(/, | - /)[0]
             // returns object with those two parameters
             return { count: count, city: city };
         })
@@ -35,13 +35,17 @@ const CityEventsChart = ({ allLocations, events }) => {
                 margin={{
                     top: 20,
                     right: 20,
-                    bottom: 20,
-                    left: 20,
+                    bottom: 60,
+                    left: -30
                 }}
             >
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="City" />
-                <YAxis type="number" dataKey="count" name="Number of events" />                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <XAxis
+                    type="category" dataKey="city" name="City"
+                    angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
+                />
+                <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                 <Scatter name="A school" data={data} fill="#8884d8" />
             </ScatterChart>
         </ResponsiveContainer>
